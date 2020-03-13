6d8777cb34d36c7aeb61c5aeada0cf4f033d467b
Sushil Maurya <39994757+sushil171@users.noreply.github.com>
2020-03-13 22:07:29 +0530

    Update router.js

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('Running');
+    res.send('Server Running !!!');
 })
 
 module.exports = router;
